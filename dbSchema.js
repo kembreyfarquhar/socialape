58d23cf6ca2c2c64adb4b0dd3925074df2be4dee
kembreyfarquhar <kembreyfarquhar@gmail.com>
2021-03-08 14:22:12 -0600

    updates db schema

--- dbSchema.js
+++ dbSchema.js
@@ -1,4 +1,16 @@
 let db = {
+  users: [
+    {
+      userId: "dhh2094rnhfh029nef929e",
+      email: "user@email.com",
+      handle: "user",
+      createdAt: "2019-03-15T11:46:01.018Z",
+      imageUrl: "image/dsfdsdfsldkjfdskj/lsjdflksnd",
+      bio: "Hello, my name is user, nice to meet you.",
+      website: "https://user.com",
+      location: "St. Louis, MO, USA",
+    },
+  ],
   screams: [
     {
       userHandle: "user",
@@ -6,6 +18,38 @@ let db = {
       createdAt: "2019-03-15T11:46:01.018Z",
       likeCount: 2,
       commentCount: 5,
+    },
+  ],
+  comments: [
+    {
+      userHandle: "user",
+      screamId: "oiajwlkfnaodiolsdng",
+      body: "nice one, mate!",
+      createdAt: "2019-03-15T11:46:01.018Z",
+    },
+  ],
+};
+
+const userData = {
+  // Redux
+  credentials: {
+    userId: "LKDFJLAJBHAOAFI0830N30NF0E9N",
+    email: "user@email.com",
+    handle: "user",
+    createdAt: "2019-03-15T11:46:01.018Z",
+    imageUrl: "image/dsfdsdfsldkjfdskj/lsjdflksnd",
+    bio: "Hello, my name is user, nice to meet you.",
+    website: "https://user.com",
+    location: "St. Louis, MO, USA",
+  },
+  likes: [
+    {
+      userHandle: "user",
+      screamId: "hfdoj2093nq0ddh0329j23",
+    },
+    {
+      userHandle: "user",
+      screamId: "0293n0ei9nc0293r2efnp09",
     },
   ],
 };
